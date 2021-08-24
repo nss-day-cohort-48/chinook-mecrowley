2ac4483f468881525190e26ff40f7a3bc9d832f5
Madeline Crowley <madelinecrowley@gmail.com>
2021-08-24 17:31:18 -0500

    top 2009 agent

--- top_2009_agent.sql
+++ top_2009_agent.sql
@@ -1,6 +1,6 @@
 SELECT
-MAX(Invoices2009.Total) AS TopSales_2009,
-Invoices2009.FullName
+MAX(EmployeeTotalSales2009.Total) AS TopSales_2009,
+EmployeeTotalSales2009.FullName
 FROM (SELECT
         Employee.FirstName || ' ' || Employee.LastName FullName,
         SUM(Invoice.Total) Total
@@ -11,4 +11,4 @@ FROM (SELECT
         ON Invoice.CustomerId = Customer.CustomerId
     WHERE InvoiceDate BETWEEN '2009-01-01' AND '2009-12-31'
     GROUP BY Employee.EmployeeId
-    ) AS Invoices2009;
+    ) AS EmployeeTotalSales2009;
