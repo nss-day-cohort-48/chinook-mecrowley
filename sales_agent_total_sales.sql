b08f1e0d11e10d49139a9a3f9529d0e4f0b886c5
Madeline Crowley <madelinecrowley@gmail.com>
2021-08-24 15:42:28 -0500

    sales agent total sales

---
+++ sales_agent_total_sales.sql
@@ -0,0 +1,9 @@
+SELECT
+Employee.FirstName || ' ' || Employee.LastName AS FullName,
+SUM(Invoice.Total) AS TotalSales
+FROM Employee
+JOIN Customer
+    ON Customer.SupportRepId = Employee.EmployeeId
+JOIN Invoice
+    ON Invoice.CustomerId = Customer.CustomerId
+GROUP BY FullName;
