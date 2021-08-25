8849e7b3abeff067344648c03803ffa7dd0e1e05
Madeline Crowley <madelinecrowley@gmail.com>
2021-08-25 10:42:24 -0500

    top 5 tracks

---
+++ top_5_tracks.sql
@@ -0,0 +1,11 @@
+SELECT
+Track.Name TrackName,
+COUNT(InvoiceLine.TrackId) TracksSold
+FROM Track
+JOIN InvoiceLine
+    ON InvoiceLine.TrackId = Track.TrackId
+JOIN Invoice
+    ON Invoice.InvoiceId = InvoiceLine.InvoiceId
+GROUP BY TrackName
+ORDER BY TracksSold DESC
+LIMIT 5;
